a22d4c00aaddbc2fbd664713800228866a011fc8
Thushan <thushan.perera95@gmail.com>
2022-07-24 15:15:39 +0800

    13.16 with bonus

---
+++ src/controllers/authors.js
@@ -0,0 +1,24 @@
+const sequelize = require('sequelize')
+const { Op } = require('sequelize')
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  let where = {}
+
+  const blogs = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('count', sequelize.col('likes')), 'articles'],
+      [sequelize.fn('sum', sequelize.col('likes')), 'likes']
+    ],
+    group: 'author',
+    order: [
+      sequelize.literal('likes DESC')
+    ]
+  })
+  res.json(blogs)
+})
+
+module.exports = router
