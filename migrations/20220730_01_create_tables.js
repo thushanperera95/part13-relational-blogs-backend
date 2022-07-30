3afdb7e798655cad157206becb2dd0b3053a9e06
Thushan <thushan.perera95@gmail.com>
2022-07-30 12:24:27 +0800

    13.17

---
+++ migrations/20220730_01_create_tables.js
@@ -0,0 +1,74 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: {
+            msg: 'Must provide a valid email for username'
+          }
+        }
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    })
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      author: {
+        type: DataTypes.STRING,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' }
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    })
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('blogs')
+    await queryInterface.dropTable('users')
+  }
+}
--- src/util/db.js
+++ src/util/db.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize')
+const { Umzug, SequelizeStorage } = require('umzug')
 const { DATABASE_URL } = require('./config')
 
 const sequelize = new Sequelize(DATABASE_URL, {
@@ -10,9 +11,33 @@ const sequelize = new Sequelize(DATABASE_URL, {
   }
 })
 
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js'
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map(mig => mig.name)
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('connected to the database')
   } catch (error) {
     console.log('failed to connect to the database')
@@ -24,5 +49,6 @@ const connectToDatabase = async () => {
 
 module.exports = {
   connectToDatabase,
-  sequelize
+  sequelize,
+  rollbackMigration
 }
