1ee21e80b10961ad27bb2b3d3e9ba4200584d420
Thushan <thushan.perera95@gmail.com>
2022-07-30 14:05:02 +0800

    13.19

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./blog')
 const User = require('./user')
+const UserBlogs = require('./userBlog')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: UserBlogs, as: 'reading_list' })
+Blog.belongsToMany(User, { through: UserBlogs, as: 'users_reading_lists' })
 
 module.exports = {
   Blog,
